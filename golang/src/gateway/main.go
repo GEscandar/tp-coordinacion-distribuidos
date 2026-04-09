a0b41f4b0f7d1a615ff604565b13fc194ea52d34
FrancoBar <fbarreneche@fi.uba.ar>
2026-04-09 20:48:48 -0300

    refactor: use struct functions and separate clientregistry

--- golang/src/gateway/main.go
+++ golang/src/gateway/main.go
@@ -1,103 +1,75 @@
 package main
 
 import (
-	"log"
-	"net"
+	"errors"
+	"log/slog"
 	"os"
-	"os/signal"
-	"sync"
-	"syscall"
+	"strconv"
 
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/messagehandler"
+	"github.com/7574-sistemas-distribuidos/tp-coordinacion/gateway"
 )
 
-type ConnectionConfig struct {
-	host string
-	port string
-}
-
-type ClientState struct {
-	ClientConnection net.Conn
-	MessageHandler   *messagehandler.MessageHandler
-}
-
-type SharedClientStates struct {
-	Lock         sync.Mutex
-	ClientStates []ClientState
-}
-
-var sharedClientStates = SharedClientStates{}
-
-func loadConnectionConfig() (*ConnectionConfig, error) {
-	connectionConfig := ConnectionConfig{
-		host: os.Getenv("SERVER_HOST"),
-		port: os.Getenv("SERVER_PORT"),
+func loadConfig() (gateway.GatewayConfig, error) {
+	inputQueueName := os.Getenv("INPUT_QUEUE")
+	if inputQueueName == "" {
+		return gateway.GatewayConfig{}, errors.New("INPUT_QUEUE environment variable is required")
 	}
 
-	return &connectionConfig, nil
+	outputQueueName := os.Getenv("OUTPUT_QUEUE")
+	if outputQueueName == "" {
+		return gateway.GatewayConfig{}, errors.New("OUTPUT_QUEUE environment variable is required")
+	}
+
+	serverHost := os.Getenv("SERVER_HOST")
+	if serverHost == "" {
+		return gateway.GatewayConfig{}, errors.New("SERVER_HOST environment variable is required")
+	}
+
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		return gateway.GatewayConfig{}, errors.New("SERVER_PORT environment variable is required")
+	}
+
+	momPort, err := strconv.Atoi(os.Getenv("MOM_PORT"))
+	if err != nil {
+		return gateway.GatewayConfig{}, errors.New("MOM_PORT environment variable is required and must be a number")
+	}
+
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return gateway.GatewayConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	return gateway.GatewayConfig{
+		InputQueueName:  inputQueueName,
+		OutputQueueName: outputQueueName,
+		ServerHost:      serverHost,
+		ServerPort:      serverPort,
+		MomHost:         momHost,
+		MomPort:         momPort,
+	}, nil
 }
 
-func handleSignals(open *bool, acceptConnection net.Listener) {
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	<-signals
-	log.Println("SIGTERM signal received")
-	*open = false
-	acceptConnection.Close()
+func run() int {
+	config, err := loadConfig()
+	if err != nil {
+		slog.Error("While loading config", "err", err)
+		return 1
+	}
+
+	server, err := gateway.NewGateway(config)
+	if err != nil {
+		slog.Error("While initializing gateway", "err", err)
+		return 1
+	}
+
+	if err := server.Run(); err != nil {
+		slog.Error("Gateway stopped with error", "err", err)
+		return 1
+	}
+	return 0
 }
 
 func main() {
-	var open bool = true
-	outputQueue, err := createQueue(os.Getenv("INPUT_QUEUE"))
-	if err != nil {
-		log.Println("While readding rabbitmq host and ports", err)
-		return
-	}
-	go outputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
-		handleClientResponse(outputQueue, msg, ack, nack)
-	})
-
-	config, err := loadConnectionConfig()
-	if err != nil {
-		log.Println("Error when reading environment variables:", err)
-		return
-	}
-
-	acceptConnection, err := net.Listen("tcp", config.host+":"+config.port)
-	if err != nil {
-		log.Println("Error when listening to connections:", err)
-		return
-	}
-	defer acceptConnection.Close()
-	go handleSignals(&open, acceptConnection)
-	log.Println("Accepting connections...")
-
-	for {
-		clientConnection, err := acceptConnection.Accept()
-		if err != nil {
-			if !open {
-				break
-			}
-			log.Println("While accepting connections: ", err)
-			return
-		}
-
-		log.Println("Client connected...")
-
-		sharedClientStates.Lock.Lock()
-		messageHandler := messagehandler.NewMessageHandler()
-		clientState := ClientState{ClientConnection: clientConnection, MessageHandler: &messageHandler}
-		sharedClientStates.ClientStates = append(sharedClientStates.ClientStates, clientState)
-		sharedClientStates.Lock.Unlock()
-
-		go handleClientRequest(clientState)
-	}
-
-	outputQueue.StopConsuming()
-	sharedClientStates.Lock.Lock()
-	for _, clientState := range sharedClientStates.ClientStates {
-		clientState.ClientConnection.Close()
-	}
-	sharedClientStates.Lock.Unlock()
+	os.Exit(run())
 }
